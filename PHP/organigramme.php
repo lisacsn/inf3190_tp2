f16152375a46f34719c6fd5a01bfc1568199a411
Léopold Maillard <leopold.ma@hotmail.fr>
2019-12-05 20:19:15 -0500

    fix icon jstree

--- PHP/organigramme.php
+++ PHP/organigramme.php
@@ -8,7 +8,7 @@ $tab = $database->select('membres',['id','fonction','prenom','nom','photo']);
 $tabfinal = '[{"id" : "CADRE", "parent" : "#", "text" : "Cadres du syndicat", "icon" : "images/logo-cadres.ico"},{"id" : "DELEGUE", "parent" : "#", "text" : "Délégués syndicaux"},{"id" : "MEMBRE", "parent" : "#", "text" : "Simples membres"}';
 
 for($i=0 ; $i<sizeof($tab) ; $i++){
-    $tabfinal = $tabfinal.',{"id" : "'. $tab[$i]["id"] . '", "parent" : "' . $tab[$i]["fonction"] . '", "text" : "' . $tab[$i]["prenom"] . ' ' . $tab[$i]["nom"] . '"}';
+    $tabfinal = $tabfinal.',{"id" : "'. $tab[$i]["id"] . '", "parent" : "' . $tab[$i]["fonction"] . '", "text" : "' . $tab[$i]["prenom"] . ' ' . $tab[$i]["nom"] . '", "icon" : "PHP/'. $tab[$i]["photo"] . '"}';
 }
 $tabfinal = $tabfinal . ']';
 
